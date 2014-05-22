2a830ae034588f64154e7c289764091d0db30312
Laurent <dang.laurent@gmail.com>
2014-05-22 18:30:04 -0400

    Add configurable.html

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,10 +1,33 @@
+var months = new Array(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
+var date = new Date();
+var now = date.getDate();
+var month = date.getMonth()+1;
+
 Pebble.addEventListener("ready", function() {
-  var phase = 4;
-  var room = 839;
-  var months = new Array(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
-  var date = new Date();
-  var now = date.getDate();
-  var month = date.getMonth()+1;
+  if (!localStorage.phase) {
+    localStorage.phase = 4;
+  }
+
+  if (!localStorage.room) {
+    localStorage.room = 839;
+  }
+  fetchData();
+});
+
+Pebble.addEventListener("showConfiguration", function() {
+  Pebble.openURL('https://dl.dropboxusercontent.com/u/22548344/configurable.html');
+});
+
+Pebble.addEventListener("webviewclosed", function(e) {
+  var options = JSON.parse(decodeURIComponent(e.response));
+  localStorage.phase = options.phase;
+  localStorage.room = options.room;
+  fetchData();
+});
+
+function fetchData() {
+  var phase = localStorage.phase;
+  var room = localStorage.room;
   var url = "http://ets-res"+phase+"-"+room+":ets"+room+"@www2.cooptel.qc.ca/services/temps/?mois="+month+"&cmd=Visualiser";
 
   var req = new XMLHttpRequest();
@@ -25,5 +48,4 @@ Pebble.addEventListener("ready", function() {
   };
 
   req.send(null);
-});
-
+}
